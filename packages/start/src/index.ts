e16578efe3eeb979f929f7aefd5fb4e88f76e171
dilan <dio4@pitt.edu>
2022-12-20 23:47:21 -0500

    init start package, mv basis to app

---
+++ packages/start/src/index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import { execa } from "@esm2cjs/execa";
+import express from 'express';
+import path from 'path';
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+const cwd = (process && process.cwd()) || __dirname;
+
+if (+major < 14) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Basis requires Node 14 or higher. Please update\n' +
+      'or switch installations to continue.'
+  );
+  process.exit(1);
+}
+
+// Boot up dev server
+
+async function main() {
+    if (process.env['IS_DEV']) {
+        console.log("Running basis in dev mode");
+        console.log("CWD:", cwd);
+        execa("pnpm tauri dev", { cwd: path.join(cwd, "packages", "app"), shell: true })
+    } else {
+        console.log("IS PROD");
+        // TODO: load to cross-platform app dir, exec from there
+    }
+}
+
+main();
---
+++ packages/start/test/index.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'expect';
+
+describe('Empty test', () => {
+    it('Empty it', () => {
+        expect(true).toBe(true)
+    })
+})
