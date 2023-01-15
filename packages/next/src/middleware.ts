a95a102c7a2fb04812995db94fca37a9b41f6c41
dilan <dio4@pitt.edu>
2023-01-15 13:43:48 -0500

    organized with middleware

--- packages/next/src/middleware.ts
+++ packages/next/src/middleware.ts
@@ -1,20 +1,25 @@
-import { NextResponse } from "next/server";
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import type { NextMiddleware } from "next/server";
 import { nanoid } from 'nanoid';
 
-export function middleware(request: NextRequest, event: NextFetchEvent) {
-
+function setHeaders(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     if (process.env.NODE_ENV === "development") { // TODO: optional prod
         requestHeaders.set("nexray-mw-url", request.url);
         requestHeaders.set("nexray-mw-id", nanoid());
     }
+    return requestHeaders;
+}
 
+export const withMiddleware = (init?: Parameters<typeof NextResponse.next>[0]): NextMiddleware => (request, event) => {
     const response = NextResponse.next({
+        ...init,
         request: {
-            headers: requestHeaders,
+            headers: setHeaders(request),
         },
     });
 
     return response;
 }
+
+export const middleware = withMiddleware();
